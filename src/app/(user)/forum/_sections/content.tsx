81c6358348b8c79e5f8a77caceafb4d0fe159a84
Mai Duc Thinh <163386482+mdt3001@users.noreply.github.com>
2025-04-25 23:13:54 +0700

    Update content.tsx

--- src/app/(user)/forum/_sections/content.tsx
+++ src/app/(user)/forum/_sections/content.tsx
@@ -4,7 +4,6 @@ import { Grid, Stack, Paper } from "@mui/material";
 import ProfileSidebar from "@/app/(user)/forum/_sections/profile-sidebar";
 import PostCreator from "@/app/(user)/forum/_sections/post-creater";
 import PostList from "@/app/(user)/forum/_sections/post-list";
-import SuggestedUsers from "@/app/(user)/forum/_sections/suggested-users";
 import { PostProvider } from "@/contexts/forum/post-context";
 import { CommentProvider } from "@/contexts/forum/comment-context"; // Nếu cần hiển thị bình luận
 
@@ -18,7 +17,7 @@ const CareerContent = () => {
             <Grid
               item
               xs={12}
-              md={3}
+              md={4}
               order={{ xs: 1, md: 1 }}
               sx={{
                 height: { xs: "250px", md: "auto" },
@@ -27,13 +26,10 @@ const CareerContent = () => {
               <ProfileSidebar />
             </Grid>
 
-            {/* Right sidebar - Suggested users */}
-            <Grid item xs={12} md={3} order={{ xs: 2, md: 3 }}>
-              <SuggestedUsers />
-            </Grid>
+
 
             {/* Main content - Posts */}
-            <Grid item xs={12} md={6} order={{ xs: 3, md: 2 }}>
+            <Grid item xs={12} md={8} order={{ xs: 3, md: 2 }}>
               <Paper sx={{ p: 2, mb: 2 }}>
                 <PostCreator />
               </Paper>
